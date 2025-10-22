5020868727ece353a3af8e3e8218597cc936bd0f
vbidarkar <vasudev.bidarkar@zalando.de>
2025-10-22 16:28:49 +0200

    Hovering cards improvements

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,10 @@
 import {Image} from "@mantine/core";
 import type {MantineSpacing} from "@mantine/core";
 
-export function Card(props: { className?: string, src: string, ml?: MantineSpacing }) {
+export function Card(props: { className?: string, src: string, ml?: MantineSpacing, style?: React.CSSProperties }) {
   return (
     <Image className={props.className}
+           style={props.style}
            bg={"transparent"}
            w={"auto"}
            mah={"325"}
--- src/components/InHandCards.tsx
+++ src/components/InHandCards.tsx
@@ -1,16 +1,32 @@
 import '../css/InHandCards.css'
 import {Card} from "./Card.tsx";
-import {Group} from "@mantine/core";
+import {Box, Divider, Group, ScrollArea} from "@mantine/core";
 
 export function InHandCards() {
   return (
-    <Group className={"in-hand-cards-row"} justify="center" align="center">
-      <Card className={"in-hand-card"} src="https://api.dunecardshub.com/uploads/images/68.png" ml={"-15%"}/>
-      <Card className={"in-hand-card"} src="https://api.dunecardshub.com/uploads/images/68.png" ml={"-15%"} />
-      <Card className={"in-hand-card"} src="https://api.dunecardshub.com/uploads/images/68.png" ml={"-15%"} />
-      <Card className={"in-hand-card"} src="https://api.dunecardshub.com/uploads/images/68.png" ml={"-15%"} />
-      <Card className={"in-hand-card"} src="https://api.dunecardshub.com/uploads/images/68.png" ml={"-15%"} />
-      <Card className={"in-hand-card"} src="https://api.dunecardshub.com/uploads/images/68.png" ml={"-15%"} />
-    </Group>
+    <div className={"in-hand-cards-row"} style={{ display: 'flex', gap: 16, padding: "16px" }}>
+      <Box className={"photo-stack in-hand-card"}>
+        <Card className="reserve-row" src="https://api.dunecardshub.com/uploads/images/68.png"/>
+        <Card className="reserve-row" src="https://api.dunecardshub.com/uploads/images/68.png"/>
+        <Card className="reserve-row" src="https://api.dunecardshub.com/uploads/images/68.png"/>
+        <Card className="reserve-row" src="https://api.dunecardshub.com/uploads/images/68.png"/>
+      </Box>
+
+      <Divider orientation="vertical" color={"gray"} />
+
+      <div style={{
+        display: 'flex',
+        gap: '16px',
+      }}>
+        <Group justify="start" align="center" style={{ overflow: 'visible' }}>
+          <Card className={"in-hand-card"} src="https://api.dunecardshub.com/uploads/images/68.png"/>
+          <Card className={"in-hand-card"} src="https://api.dunecardshub.com/uploads/images/68.png" ml={"-15%"} />
+          <Card className={"in-hand-card"} src="https://api.dunecardshub.com/uploads/images/68.png" ml={"-15%"} />
+          <Card className={"in-hand-card"} src="https://api.dunecardshub.com/uploads/images/68.png" ml={"-15%"} />
+        </Group>
+      </div>
+
+    </div>
+
   )
 }
