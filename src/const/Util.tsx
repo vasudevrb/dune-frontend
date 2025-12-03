3d949fe0bdd0b783faf7d51f7431b8c20579319c
vbidarkar <vasudev.bidarkar@zalando.de>
2025-12-03 13:33:42 +0100

    Add static sardaukar commanders

--- src/const/Util.tsx
+++ src/const/Util.tsx
@@ -45,7 +45,7 @@ export const gameStartState: GameModel = {
     haggaBasin: 0,
     imperialBasin: 0
   },
-  contracts: [
+  currentContracts: [
     "http://localhost:8080/contracts/contract_15.png",
     "http://localhost:8080/contracts/contract_17.png"
   ],
